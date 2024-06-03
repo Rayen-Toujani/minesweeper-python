2912543cd50981f71d38c99d7fc592d630157b15
Mohamed Rayen Toujani <mo.rayen.toujani@gmail.com>
2024-06-03 18:04:34 +0100

    finished game

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
+import minesweeper
 
 import random
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Minesweeper")
+    game = minesweeper.Minesweeper(root)
+    root.mainloop()
